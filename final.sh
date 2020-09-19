54e9c4f4f1d3c65fd64ff3bc86ab11c3f3b49fc3
Zaken <61211705+Promoviespro@users.noreply.github.com>
2020-09-19 20:37:42 +0300

    Update final.sh

--- final.sh
+++ final.sh
@@ -60,6 +60,9 @@ chown -R nginx:nginx /var/lib/phpMyAdmin/upload/
 wget --no-check-certificate https://raw.githubusercontent.com/skurudo/phpmyadmin-fixer/master/pma-centos.sh && chmod +x pma-centos.sh && ./pma-centos.sh
 cd /usr/share/phpMyAdmin/themes/ && wget --no-check-certificate https://files.phpmyadmin.net/themes/fallen/0.7/fallen-0.7.zip && unzip fallen-0.7.zip && rm -rf fallen-0.7.zip
 
+cd / && wget -q https://repo.b-cdn.net/proxy-confs.zip && unzip -o proxy-confs.zip && rm -f proxy-confs* 
+rm -f /etc/nginx/conf.d/*
+
 wget https://raw.githubusercontent.com/Promoviespro/install/master/cdn.conf -O /etc/nginx/conf.d/cdn.conf
 wget https://raw.githubusercontent.com/Promoviespro/install/master/cdn-ssl.conf -O /etc/nginx/conf.d/cdn-ssl.conf
 sed -i 's/promoviesonline.com/promovies3d.com/g' /etc/nginx/conf.d/*.conf
